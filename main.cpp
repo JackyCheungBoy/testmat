42a8bb669f9a4d88628f487dcfa5194e1a288367
Sherlock <Sherlock@DESKTOP-OUGK3CN>
2017-05-08 21:04:36 +0800

    增加step和ptr遍历

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace cv;
 ************************/
 void PrintMs(const char *text = " ")
 {
-	static long long last = 0;
+	static long long last = 0;      //
 	long long cur = getTickCount();
 	if (last == 0)
 	{
@@ -31,7 +31,7 @@ int main(int argc, char *argv)
 	int  mat_size = mat.elemSize();
 	int  size = mat.rows * mat.cols *mat_size;
 	PrintMs();
-
+	//遍历连续的mat空间
 	for (int i = 0; i < size; i += mat_size)
 	{
 		mat.data[i] = 255; //blue
@@ -41,6 +41,35 @@ int main(int argc, char *argv)
 	}
 
 	PrintMs("mat.data ms");
+
+	// 直接地址遍历不一定连续的空间
+	for (int row = 0; row < mat.rows; row++)
+	{
+		for (int col = 0; col < mat.cols; col++)
+		{
+			(&mat.data[row*mat.step])[col*mat_size] = 0; //B
+			(&mat.data[row*mat.step])[col*mat_size+1] = 0;
+			(&mat.data[row*mat.step])[col*mat_size+2] = 255;
+
+		}
+	}
+	PrintMs("mat.step ms");
+
+	// 通过mat.ptr 接口遍历mat
+	for (int row = 0; row < mat.rows; row++)
+	{
+		for (int col = 0; col < mat.cols; col++)
+		{
+			Vec3b *c = mat.ptr<Vec3b>(row, col);
+			c->val[0] = 0;
+			c->val[1] = 255;
+			c->val[2] = 0;
+
+
+		}
+	}
+	PrintMs("mat.ptr ms");
+
 	namedWindow(" mat");  //创建一个窗口，
 	imshow("mat", mat);   // 显示这个窗口
 	waitKey(0);
